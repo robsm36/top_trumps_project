14878e2b8d3c22a0ce7233065bcf1c4fb4e5533e
Robson Miranda <robsm36@gmail.com>
2022-09-28 01:37:17 -0300

    Requisito 8

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ class App extends React.Component {
     cardAttr3: '0',
     cardImage: '',
     cardRare: 'normal',
-    savedCards: [],
     cardTrunfo: false,
+    savedCards: [],
     isSaveButtonDisabled: true,
     hasTrunfo: false,
   };
@@ -80,7 +80,7 @@ class App extends React.Component {
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2,
       cardAttr3, cardImage, cardRare, cardTrunfo,
-      isSaveButtonDisabled, hasTrunfo } = this.state;
+      isSaveButtonDisabled, hasTrunfo, savedCards } = this.state;
 
     return (
       <div>
@@ -109,6 +109,19 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
         />
+        {
+          savedCards.map((card) => (<Card
+            key={ card.cardName }
+            cardName={ card.cardName }
+            cardDescription={ card.cardDescription }
+            cardAttr1={ card.cardAttr1 }
+            cardAttr2={ card.cardAttr2 }
+            cardAttr3={ card.cardAttr3 }
+            cardImage={ card.cardImage }
+            cardRare={ card.cardRare }
+            cardTrunfo={ card.cardTrunfo }
+          />))
+        }
       </div>
     );
   }
